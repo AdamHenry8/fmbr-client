64b5df448fadfd20b64f1afedc54e29da4122d15
Adam Ben Nun <adambennun8@gmail.com>
2022-01-03 09:53:00 +0200

    05

--- src/Components/NewsComp/News.js
+++ src/Components/NewsComp/News.js
@@ -32,7 +32,7 @@ function NewsComp() {
                   <Link to={"/newsItem/" + item._id}>
                     <div id="release_card_wrapper" class="image">
                       <img key={item._id} src={item.image} alt="news_image" class="image_img" className='news_item_img'></img>
-                      <div class="image_overlay" style={{overflow: 'hidden'}}>
+                      <div class="image_overlay" style={{height: '380px'}}>
                           <h4 class="image_title">{item.headline}</h4> 
                           <h4 class="image_title">{item.subheading} </h4> 
                           <p class="image_description" style={{textTransform: 'none', padding: '20px', textAlign: 'center'}}>{item.body}</p>
