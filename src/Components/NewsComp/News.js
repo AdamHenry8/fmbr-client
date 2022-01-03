3bf032dbea75363e7c3123116df9293c9d0ce48c
Adam Ben Nun <adambennun8@gmail.com>
2022-01-03 08:58:04 +0200

    news image overlay to replace card

--- src/Components/NewsComp/News.js
+++ src/Components/NewsComp/News.js
@@ -1,6 +1,7 @@
 import {useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import utils from '../../Services/NewsUtils';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -27,24 +28,36 @@ function NewsComp() {
   <div class="news_container">
     {
     news.map(item => {
-    return  <div key={item._id} class="news_item" onClick={() => navToNewsItem(item._id)}>
-              <h3 className="news_headline"> {item.headline} </h3>
-              <p className="news_subheading">{item.subheading}</p> 
-
-              <div class="img_text_wrapper">
-                <img src={item.image} alt="news-img" class="news_img"></img>
-                <div className='item_body_wrapper'>
-                  {item.body.length > 70 ?
-                    (
-                    <div className="news_item_body">
-                      {`${item.body.substring(0, 70)}...`}<span className='underlined'>read more</span>
+     return <div className="flex_item_wrapper">
+                  <Link to={"/newsItem/" + item._id}>
+                    <div id="release_card_wrapper" class="image">
+                      <img key={item._id} src={item.image} alt="news_image" class="image_img"></img>
+                      <div class="image_overlay">
+                          <h4 class="image_title">{item.headline}</h4> 
+                          <h4 class="image_title">{item.subheading} </h4> 
+                          <p class="image_description">{item.body}</p>
+                      </div>
                     </div>
-                    ) :
-                    <p className="news_item_body">{item.body}</p>
-                  }
-                </div>
-              </div>     
-            </div>       
+                  </Link>
+              </div>           
+    // return  <div key={item._id} class="news_item" onClick={() => navToNewsItem(item._id)}>
+    //           <h3 className="news_headline"> {item.headline} </h3>
+    //           <p className="news_subheading">{item.subheading}</p> 
+
+    //           <div class="img_text_wrapper">
+    //             <img src={item.image} alt="news-img" class="news_img"></img>
+    //             <div className='item_body_wrapper'>
+    //               {item.body.length > 70 ?
+    //                 (
+    //                 <div className="news_item_body">
+    //                   {`${item.body.substring(0, 70)}...`}<span className='underlined'>read more</span>
+    //                 </div>
+    //                 ) :
+    //                 <p className="news_item_body">{item.body}</p>
+    //               }
+    //             </div>
+    //           </div>     
+    //         </div>       
     })
     }
   </div>
