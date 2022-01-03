47994c8ac20ac78672c36e7b2b0c212a1bd76159
Adam Ben Nun <adambennun8@gmail.com>
2022-01-03 10:13:58 +0200

    16

--- src/Components/HomeComp/Home.js
+++ src/Components/HomeComp/Home.js
@@ -2,7 +2,7 @@ import React , {useState, useEffect } from 'react';
 import releaseUtils from '../../Services/ReleaseUtils';
 import newsUtils from '../../Services/NewsUtils';
 import  {Link} from 'react-router-dom';
-// import { useHistory } from 'react-router-dom';
+
 
 import '../ReleasesComp/styles.css'; 
 import './styles.css';
@@ -11,7 +11,7 @@ function HomeComp() {
 
   const [latestReleases, setLatestReleases] = useState([]);
   const [latestNews, setLatestNews] = useState([]);
-  // const history = useHistory();
+
 
   useEffect(() =>{
     async function fetchData()
@@ -27,9 +27,6 @@ function HomeComp() {
     fetchData()  
   }, [])
 
-  // const navToNewsItem = (id) => {
-  //   history.push('/newsItem/'+ id);
-  // }
  
   return (
   <div id="home_comp">
@@ -71,27 +68,6 @@ function HomeComp() {
                     </div>
                   </Link>
                 </div>           
-        
-        
-        
-        
-        // <div key={item._id} class="news_item" onClick={() => navToNewsItem(item._id) }>
-        //             <h3 className="news_headline"> {item.headline} </h3>
-        //             <p className="news_subheading">{item.subheading}</p> 
-        //             <div class="img_text_wrapper">
-        //               <img src={item.image} alt="news-img" class="news_img"></img>
-        //               <div className='item_body_wrapper'>
-        //                 {item.body.length > 70 ?
-        //                   (
-        //                     <div className="news_item_body">
-        //                       {`${item.body.substring(0, 70)}...`}<span className='underlined'>read more</span>
-        //                     </div>
-        //                   ) :
-        //                   <p className="news_item_body">{item.body}</p>
-        //                 }
-        //               </div>
-        //             </div> 
-        //           </div>
         })
       } 
       
--- src/Components/NewsComp/News.js
+++ src/Components/NewsComp/News.js
@@ -1,5 +1,4 @@
 import {useState, useEffect } from 'react';
-// import { useHistory } from 'react-router-dom';
 import utils from '../../Services/NewsUtils';
 import { Link } from 'react-router-dom';
 
@@ -8,7 +7,6 @@ import './styles.css';
 function NewsComp() {
 
   const [news, setNews] = useState([]);
-  // const history = useHistory();
 
   useEffect(() =>{
     const fetchData = async () =>
@@ -20,9 +18,6 @@ function NewsComp() {
     fetchData();
   }, [])
 
-  // const navToNewsItem = (id) => {
-  //   history.push('/newsItem/'+ id);
-  // }
  
   return (
   <div class="news_container">
@@ -39,25 +34,7 @@ function NewsComp() {
                       </div>
                     </div>
                   </Link>
-              </div>           
-    // return  <div key={item._id} class="news_item" onClick={() => navToNewsItem(item._id)}>
-    //           <h3 className="news_headline"> {item.headline} </h3>
-    //           <p className="news_subheading">{item.subheading}</p> 
-
-    //           <div class="img_text_wrapper">
-    //             <img src={item.image} alt="news-img" class="news_img"></img>
-    //             <div className='item_body_wrapper'>
-    //               {item.body.length > 70 ?
-    //                 (
-    //                 <div className="news_item_body">
-    //                   {`${item.body.substring(0, 70)}...`}<span className='underlined'>read more</span>
-    //                 </div>
-    //                 ) :
-    //                 <p className="news_item_body">{item.body}</p>
-    //               }
-    //             </div>
-    //           </div>     
-    //         </div>       
+              </div>             
     })
     }
   </div>
